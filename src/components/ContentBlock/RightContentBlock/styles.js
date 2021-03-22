58daf4cda256507e5370b067736cfefb5a79e1be
Mano <manogajapathi@gmail.com>
2021-03-22 11:19:38 +0530

    css fix

--- src/components/ContentBlock/RightContentBlock/styles.js
+++ src/components/ContentBlock/RightContentBlock/styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const RightBlockContainer = styled.section`
   position: relative;
-  padding: 10rem 0 8rem;
-
+  padding: 16rem 0 8rem;
+  
   @media only screen and (max-width: 768px) {
-    padding: 4rem 0 0rem;
+    padding: 8rem 0 0rem;
   }
 `;
 
